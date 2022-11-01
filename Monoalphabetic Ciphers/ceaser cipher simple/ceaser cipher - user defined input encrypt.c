9738cddeafe08782446e29d1333ba49633c59942
Anuj Shrestha <anujshrestha189@gmail.com>
2022-11-01 23:24:40 +0545

    ceaser cipher simple added

---
+++ Monoalphabetic Ciphers/ceaser cipher simple/ceaser cipher - user defined input encrypt.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main(){
+	int i,j,n,key = 3;
+	char str[12];
+	
+	printf("Enter a plaintext\n");
+	scanf("%[^\n]",str);
+	
+	for(i=0;i<11;i++){
+		
+		if(str[i] == 32){
+			printf(" ");
+		}else{
+			for(j=0;j<26;j++){
+				
+				if(str[i] == (j+97)){
+					n = (j+key) % 26;
+					break;
+				}
+			}
+			printf("%c",(n+97));
+		}	
+	}
+	
+	return 0;
+}
+
